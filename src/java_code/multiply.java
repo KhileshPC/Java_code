419e2f0f0b093ac3db13135e0b335bddd8198b78
Khilesh Chaudhari <Khilu0707@gmail.com>
2022-08-11 16:50:07 +0530

    added multiply.java

---
+++ src/java_code/multiply.java
@@ -0,0 +1,14 @@
+package java_code;
+
+public class multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int  b=6;
+		
+		System.out.println(a*b);
+
+	}
+
+}
